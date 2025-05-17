d3e054dbedc66755cb6c86c0f0f076151327ec32
Dhruv <dhruvkulsh@gmail.com>
2025-05-17 22:31:55 +0530

    api changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import { Screen } from "./Screen"
 
 export const App = () => {
   const api = "https://api.freeapi.app/api/v1/public/youtube/videos";
+
+  const [apiData, setApiData] = useState(null);
+
+  const fetchData = async () => {
+
+    try {
+      const res = await fetch(api);
+      const data = await res.json();
+      console.log(data);
+      setApiData[data];
+    } catch(error) {
+      console.log(error.message);
+
+    }
+  }
+
+  fetchData();
   return (
     <>
       <Screen />
+      {apiData}
     </>
   )
 }
