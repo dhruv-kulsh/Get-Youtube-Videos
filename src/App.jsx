8aef112ba5dc36d0fde7ed5b830f9167a96aedda
Dhruv <dhruvkulsh@gmail.com>
2025-05-31 22:10:15 +0530

    use context

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,10 @@ import { globalContext } from "./context";
 
 export const App = () => {
 
-  const { data } = globalContext();
-  const {api, count, number} = data;
-
-  // console.log(data);
-  console.log(api);  
+  const { data, apiData } = globalContext();
+  const {count, number} = data;
 
 
-  const fetchData = async () => {
-
-    try {
-      const res = await fetch(api);
-      const data = await res.json();      
-      console.log(data);
-    } catch (error) {
-      console.log(error.message);
-
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    // console.log(api);
-  }, [api]);
 
   return (
     <>
@@ -37,7 +15,7 @@ export const App = () => {
       <h1>Title</h1>
       {count + 1}
       {
-        api?.data?.data?.map((datapoint) => {
+        apiData?.data?.data?.map((datapoint) => {
           return (
             < ul key = { datapoint.items.id } >
               <li>{datapoint.items.snippet.title}</li>
--- src/context.jsx
+++ src/context.jsx
@@ -1,19 +1,34 @@
-import { createContext, useReducer, useContext, useState } from "react";
+import { createContext, useReducer, useContext, useState, useEffect } from "react";
 
 const appContext = createContext();
 
 const AppProvider = ({ children }) => {
+    const api = "https://api.freeapi.app/api/v1/public/youtube/videos";
 
     const initialData = {
-        api: "https://api.freeapi.app/api/v1/public/youtube/videos",
         count: 0,
         number: 1
     };
     const [data, setData] = useState(initialData);
+    const [apiData, setApiData] = useState(null);
+
+    const apiCall = async () => {
+        try {
+            const res = await fetch(api);
+            const data = await res.json();
+            setApiData(data);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        apiCall();
+    }, []);
 
     return (
         <>
-            <appContext.Provider value={{ data }}>{children}</appContext.Provider>
+            <appContext.Provider value={{ data, apiData }}>{children}</appContext.Provider>
         </>
     )
 }
@@ -24,4 +39,4 @@ const globalContext = () => {
 
 export { globalContext };
 
-export {AppProvider}
+export { AppProvider }
